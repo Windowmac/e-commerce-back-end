2465a5ac35274223b8ecc411010d40f3ecef5cb8
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-07 22:55:53 -0700

    added model associations and category-routes

--- models/index.js
+++ models/index.js
@@ -4,13 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Tag, { through: 'tag_id' });
+Tag.belongsToMany(ProductTag, { through: 'tag_id' });
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,69 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  const allCategories = await Category.findAll({
+    include: [{ model: Product }],
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+  res.status(200).json(allCategories);
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  const category = await Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+
+  if (!category) {
+    res.status(404).json('no category founc!');
+  } else {
+    res.status(200).json(category);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const newCategory = await Category.create(req.body).catch((err) => {
+    res.status(400).json(err);
+  });
+  res.status(201).json(newCategory);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  const category = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+
+  if (!category) {
+    res.status(404).json('no category found!');
+  } else {
+    res.status(202).end();
+  }
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const destroyedCategory = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).catch(err => {
+    res.status(500).json(err);
+  });
+
+  if(!destroyedCategory) {
+    res.status(404).json('no category found!');
+  } else {
+    res.status(202).json('category deleted!');
+  }
 });
 
 module.exports = router;
